dc76cbed24e65f69149d822438d178f9ba01a56c
SibabalweSitolotolo <sibabalwesitolotolo@gmail.com>
2024-11-17 19:25:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
 
 
+let userInput = document.getElementById("date");
+userInput.max = new Date().toISOString().split("T")[0];
+let result = document.getElementById("result");
 
+function CalculateAge(){
+    let birthdate = new Date(userInput.value);
 
-function enter() {
-    const bornYear = document.getElementById('date1').value;
-    const currentYear = document.getElementById('date2').value;
-    results = document.getElementById('results')
-   
+    let d1 = birthdate.getDate();
+    let m1 = birthdate.getMonth() + 1;
+    let y1 = birthdate.getFullYear();
 
+    let today = new Date();
 
-    let x = currentYear - bornYear
-   
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
 
-    return results.innerHTML = (x)
+    let d3, m3, y3;
 
-   
+    y3 = y2 - y1;
+
+    if(m2 >= m1){
+        m3 = m2 - m1;
+    }else{
+        y3 --;
+        m3 = 12 + m2 - m1;
+
+    }
+
+    if (d2 >= d1){
+        d3 = d2 - d1;
+
+    }else{
+        m3--;
+        d3 = getDaysInMonth(y1, m1) + d2 - d1
+    }
+    if (m3 < 0){
+        m3 = 11;
+        y3--;
+    }
+    result.innerHTML = `You are <span> ${y3} </span> years, <span> ${m3} </span> months and <span> ${d3} </span> days old`;
+    console.log(y3,m3,d3)
+
 }
-// enter()
+
+function getDaysInMonth(year, month){
+    return new Date (year, month, 0).getDate();
+}
